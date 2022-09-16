b01a2aae89f54a4469fd54a61755d8b57fee673c
a.rezazadeh <a.rezazadeh@ronad.ir>
2022-09-16 19:03:06 +0430

    add event-driven-patterns project

---
+++ src/main/java/learning/eventDrivenPatterns/EventDrivenPatternsApplication.java
@@ -0,0 +1,13 @@
+package learning.eventDrivenPatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenPatternsApplication.class, args);
+    }
+
+}
